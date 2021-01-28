576d1708755f27e8abab5fabe722711dd9c1340a
Sinan <tur595@hotmail.com>
2021-01-28 15:50:52 +0100

    added ui elements and basic styling

--- musicplayer/src/App.js
+++ musicplayer/src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+//import styles
+import "./styles/app.scss";
+//components
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
