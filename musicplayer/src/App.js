18969763f8aa9dce4d3eb556a8918b802060a553
Sinan Senevrensel <tur595@hotmail.com>
2021-01-29 13:04:01 +0100

    added set of songs and data retrieval for text fields

--- musicplayer/src/App.js
+++ musicplayer/src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 //import styles
 import "./styles/app.scss";
 //components
 import Player from "./components/Player";
 import Song from "./components/Song";
+import data from "./data";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
