42d773e52e7d7061bca258a2aa5dfd1461ed4c37
Sinan Senevrensel <tur595@hotmail.com>
2021-02-01 18:46:51 +0100

    added styling for each component

--- capture-website/src/components/GlobalStyle.js
+++ capture-website/src/components/GlobalStyle.js
@@ -9,7 +9,8 @@ const GlobalStyle = createGlobalStyle`
 }
 
 body{
-    background: #1b1b1b
+    background: #1b1b1b;
+    font-family: "Inter", sans-serif;
 }
 
 button{
@@ -21,6 +22,7 @@ button{
     background: transparent;
     color:white;
     transition: all 0.5s ease;
+    font-family: "Inter", sans-serif;
     &:hover{
         background-color: #23d997;
         color: white;
@@ -36,6 +38,7 @@ h2{
     }
     h4{
         font-weight: bold;
+        font-size: 2rem;
     }
     span{
         font-weight: bold;
