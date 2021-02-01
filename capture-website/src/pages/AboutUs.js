4ceb0848e3b1246f83e406d379aa4f5acff1169a
Sinan Senevrensel <tur595@hotmail.com>
2021-02-01 18:58:43 +0100

    added nav bar and its styling

--- capture-website/src/pages/AboutUs.js
+++ capture-website/src/pages/AboutUs.js
@@ -3,10 +3,12 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
+import Nav from "../components/Nav";
 
 const AboutUs = () => {
   return (
     <>
+      <Nav />
       <AboutSection />
       <ServicesSection />
       <FaqSection />
