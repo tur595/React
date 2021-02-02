2c102a439591eae970c96e96d8af70af5ef629e6
Sinan Senevrensel <tur595@hotmail.com>
2021-02-02 15:43:14 +0100

    added routing and basic styling to our work page

--- capture-website/src/App.js
+++ capture-website/src/App.js
@@ -1,14 +1,30 @@
 import React from "react";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 //import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- capture-website/src/pages/AboutUs.js
+++ capture-website/src/pages/AboutUs.js
@@ -3,12 +3,10 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
-import Nav from "../components/Nav";
 
 const AboutUs = () => {
   return (
     <>
-      <Nav />
       <AboutSection />
       <ServicesSection />
       <FaqSection />
