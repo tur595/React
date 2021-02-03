cd221f72400d3ce326c36c8ee4974b7798458eab
Sinan Senevrensel <tur595@hotmail.com>
2021-02-03 12:54:37 +0100

    added page transitions and element animations

---
+++ capture-website/src/animation.js
@@ -0,0 +1,47 @@
+export const pageAnimation = {
+  hidden: {
+    opacity: 0,
+    y: 300,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
+    },
+  },
+  exit: {
+    opacity: 0,
+    //x: 300,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+export const titleAnim = {
+  hidden: { opacity: 0, x: 200 },
+  show: { opacity: 1, x: 0, transition: { duration: 1, ease: "easeOut" } },
+};
+
+export const fade = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { ease: "easeOut", duration: 0.75 },
+  },
+};
+
+export const photoAnim = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
+    },
+  },
+};
