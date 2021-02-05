ee39542557e4504b851df36c55df6f477b0fe73a
Sinan Senevrensel <tur595@hotmail.com>
2021-02-05 12:12:55 +0100

    added scroll reset nav bar behaviour and contact us styling

--- capture-website/src/pages/OurWork.js
+++ capture-website/src/pages/OurWork.js
@@ -16,6 +16,7 @@ import {
   sliderContainer,
 } from "../animation";
 import { useScroll } from "../components/useScroll";
+import ScrollTop from "../components/ScrollTop";
 
 const OurWork = () => {
   const [element, controls] = useScroll();
@@ -67,6 +68,7 @@ const OurWork = () => {
           <img src={goodtimes} alt="goodtimes"></img>
         </Link>
       </Movie>
+      <ScrollTop />
     </Work>
   );
 };
