eb3dba038ad679383e424e62deb03deedfb299b2
Sinan Senevrensel <tur595@hotmail.com>
2021-03-02 19:08:54 +0100

    added conditions for operators

--- iphone-calculator/src/components/App/index.js
+++ iphone-calculator/src/components/App/index.js
@@ -1,33 +1,155 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Button from "../Button";
 import "../Button/Button.css";
 
 const App = () => {
+  const [value, setValue] = useState("0");
+  const [memory, setMemory] = useState(null);
+  const [operator, setOperator] = useState(null);
+
+  const handleButtonPress = (content) => () => {
+    const num = parseFloat(value);
+
+    if (content === "AC") {
+      setValue("0");
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+    if (content === "±") {
+      setValue((num * -1).toString());
+      return;
+    }
+
+    if (content === "%") {
+      setValue((num / 100).toString());
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+
+    if (content === "+") {
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
+      setValue("0");
+      setOperator("+");
+      return;
+    }
+    if (content === "−") {
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
+      setValue("0");
+      setOperator("−");
+      return;
+    }
+    if (content === "×") {
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
+      setValue("0");
+      setOperator("×");
+      return;
+    }
+    if (content === "÷") {
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
+      setValue("0");
+      setOperator("÷");
+      return;
+    }
+
+    if (content === "=") {
+      if (!operator) return;
+
+      if (operator === "+") {
+        setValue((memory + parseFloat(value)).toString());
+      } else if (operator === "−") {
+        setValue((memory - parseFloat(value)).toString());
+      } else if (operator === "×") {
+        setValue((memory * parseFloat(value)).toString());
+      } else if (operator === "÷") {
+        setValue((memory / parseFloat(value)).toString());
+      }
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+
+    setValue(parseFloat(num + content).toString());
+  };
+
   return (
     <div className="App">
       <div className="top">4:43</div>
-      <div className="display">0</div>
+      <div className="display">{value}</div>
       <div className="buttons">
-        <Button content="AC" />
-        <Button content="±" />
-        <Button content="%" />
-        <Button content="÷" />
-        <Button content="7" />
-        <Button content="8" />
-        <Button content="9" />
-        <Button content="×" />
-        <Button content="4" />
-        <Button content="5" />
-        <Button content="6" />
-        <Button content="−" />
-        <Button content="1" />
-        <Button content="2" />
-        <Button content="3" />
-        <Button content="+" />
-        <Button content="0" />
-        <Button content="." />
-        <Button content="=" />
+        <Button
+          onButtonClick={handleButtonPress}
+          content="AC"
+          type="function"
+        />
+        <Button onButtonClick={handleButtonPress} content="±" type="function" />
+        <Button onButtonClick={handleButtonPress} content="%" type="function" />
+        <Button onButtonClick={handleButtonPress} content="÷" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="7" />
+        <Button onButtonClick={handleButtonPress} content="8" />
+        <Button onButtonClick={handleButtonPress} content="9" />
+        <Button onButtonClick={handleButtonPress} content="×" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="4" />
+        <Button onButtonClick={handleButtonPress} content="5" />
+        <Button onButtonClick={handleButtonPress} content="6" />
+        <Button onButtonClick={handleButtonPress} content="−" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="1" />
+        <Button onButtonClick={handleButtonPress} content="2" />
+        <Button onButtonClick={handleButtonPress} content="3" />
+        <Button onButtonClick={handleButtonPress} content="+" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="0" />
+        <Button onButtonClick={handleButtonPress} content="." />
+        <Button onButtonClick={handleButtonPress} content="=" type="operator" />
       </div>
       <div className="bottom">-</div>
     </div>
--- iphone-calculator/src/components/Button/index.js
+++ iphone-calculator/src/components/Button/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./Button.css";
 
-const Button = ({ content }) => {
+const Button = ({ content, type, onButtonClick }) => {
   return (
-    <div className={`button ${content === "0" ? "zero" : ""}`}>{content}</div>
+    <div
+      className={`button ${content === "0" ? "zero" : ""}
+    ${type || ""}`}
+      onClick={onButtonClick(content)}
+    >
+      {content}
+    </div>
   );
 };
 
