5cf887369ca458b7c040980eb13571ffc32c364d
Sinan <tur595@hotmail.com>
2021-03-06 11:17:40 +0100

    design tweet card

--- tweet-streaming/src/components/Tweet.js
+++ tweet-streaming/src/components/Tweet.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const Tweet = () => {
   return (
-    <div className="tweet">
-      <h1>Tweet</h1>
+    <div className="tweet card my-4">
+      <div className="card-body">
+        <h5 className="card-title">today is a good day</h5>
+        <h6 className="card-subtitle mb-2 text-muted">John Doe</h6>
+        <a href="https://twitter.com" className="btn btn-primary mt-3">
+          <i className="fab fa-twitter"></i> Go To Tweet
+        </a>
+      </div>
     </div>
   );
 };
