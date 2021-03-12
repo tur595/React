99188f702c0a483d4efa426ed330de0aed21599d
Sinan Senevrensel <tur595@hotmail.com>
2021-03-12 16:13:59 +0100

    chat component and styling

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+const config = require("dotenv").config();
+const PROJECTID = process.env.PROJECT_ID;
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Chat App</h1>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="d6ccb23d-0d3d-4be9-b4f9-3acbcff0f477"
+      userName="johndoe"
+      userSecret="12345"
+    />
   );
 };
 
