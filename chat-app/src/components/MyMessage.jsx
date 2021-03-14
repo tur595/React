0b8c19607136a08f4774aaa7cea2059c91bc066d
Sinan Senevrensel <tur595@hotmail.com>
2021-03-14 14:53:06 +0100

    setup mymessage component

--- chat-app/src/components/MyMessage.jsx
+++ chat-app/src/components/MyMessage.jsx
@@ -1,5 +1,27 @@
-const MyMessage = () => {
-  return <div>MyMessage</div>;
+const MyMessage = ({ message }) => {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt="message-attachment"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#634786",
+      }}
+    >
+      {message.text}
+    </div>
+  );
 };
 
 export default MyMessage;
