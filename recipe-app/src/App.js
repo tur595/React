fb9e712fb43c04b36364d2676da8be900746d95f
Sinan Senevrensel <tur595@hotmail.com>
2021-03-22 20:36:07 +0100

    get recipes
    set environment variables

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import env from "react-dotenv";
 
 function App() {
-  return <div className="App">Recipe App</div>;
+
+  useEffect(() => {
+    getRecipes();
+  })
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${env.APP_ID}&app_key=${env.APP_KEY}`)
+    const data = await response.json();
+    console.log(data)
+  }
+
+  return <div className="App">
+    <form className="search-form">
+      <input className="search-bar" type="text"/>
+      <button className="search-button" type="submit">
+        Search
+      </button>
+    </form>
+  </div>;
 }
 
 export default App;
