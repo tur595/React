a70b82b079cbb479d9eda5beb3df6b0734ce41c9
Sinan Senevrensel <tur595@hotmail.com>
2021-03-24 15:00:50 +0100

    styling and ingredients

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,39 +1,61 @@
 import React, { useEffect, useState } from "react";
 import Recipe from "./Recipe";
 import env from "react-dotenv";
+import "./App.css";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-  });
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${env.APP_ID}&app_key=${env.APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${env.APP_ID}&app_key=${env.APP_KEY}`
     );
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
   };
 
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <h1 className="title">Top Notch Recipes!</h1>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
-      {recipes.map((recipe) => (
-        <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-        />
-      ))}
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- recipe-app/src/Recipe.js
+++ recipe-app/src/Recipe.js
@@ -1,11 +1,17 @@
 import React from "react";
+import style from "./recipe.module.css";
 
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
-      <p>{calories}</p>
-      <img src={image} alt="" />
+      <p>{Math.round(calories)} kcal</p>
+      <img className={style.image} src={image} alt="" />
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
     </div>
   );
 };
