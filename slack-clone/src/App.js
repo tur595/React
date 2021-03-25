6b3674c9594f5007ccde62026867e7a12a7f7358
Sinan Senevrensel <tur595@hotmail.com>
2021-03-25 17:31:15 +0100

    styling dependencies
    header component

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Slack clone</h1>
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
---
+++ slack-clone/src/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <h1>Header</h1>
+      {/* Header left */}
+
+      {/* Header search */}
+
+      {/* Header left */}
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  h1 {
+    color: red;
+  }
+`;
