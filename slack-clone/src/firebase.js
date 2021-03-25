1a8c5b71e64622a2db3a9c319ce11861994bab85
Sinan Senevrensel <tur595@hotmail.com>
2021-03-25 17:02:55 +0100

    initial commit

---
+++ slack-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDT_s4rJL1T383xuTT-MCJiu11ydsgzf8w",
+  authDomain: "slack-clone-299f9.firebaseapp.com",
+  projectId: "slack-clone-299f9",
+  storageBucket: "slack-clone-299f9.appspot.com",
+  messagingSenderId: "544633336606",
+  appId: "1:544633336606:web:ed3a29803009ed57884f0f",
+};
