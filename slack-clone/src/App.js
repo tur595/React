42080cdad3d221dd0bac5e5516dc2fb41aabba66
Sinan Senevrensel <tur595@hotmail.com>
2021-03-29 21:38:21 +0200

    login component

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -5,11 +5,17 @@ import styled from "styled-components";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Login from "./components/Login";
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <div className="app">
       <Router>
+        {!user ? <Login /> : ""}
         <>
           <Header />
           <AppBody>
