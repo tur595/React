b6b53e757f9471d45b9599dd13e92191021bb476
Sinan Senevrensel <tur595@hotmail.com>
2021-04-06 15:24:13 +0200

    render cards

--- netflix/src/components/card/index.js
+++ netflix/src/components/card/index.js
@@ -1,4 +1,22 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useContext } from "react";
+
+import {
+  Container,
+  Group,
+  Title,
+  SubTitle,
+  Text,
+  Feature,
+  FeatureTitle,
+  FeatureText,
+  FeatureClose,
+  Maturity,
+  Content,
+  Meta,
+  Entities,
+  Item,
+  Image,
+} from "./styles/card";
 
 export const FeatureContext = createContext();
 
@@ -23,8 +41,8 @@ Card.Title = function CardTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
 };
 
-Card.Subtitle = function CardSubtitle({ children, ...restProps }) {
-  return <Subtitle {...restProps}>{children}</Subtitle>;
+Card.SubTitle = function CardSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
 };
 
 Card.Text = function CardText({ children, ...restProps }) {
@@ -33,6 +51,37 @@ Card.Text = function CardText({ children, ...restProps }) {
 
 Card.Meta = function CardMeta({ children, ...restProps }) {
   return <Meta {...restProps}>{children}</Meta>;
+};
+
+Card.Feature = function CardFeature({ children, category, ...restProps }) {
+  const { showFeature, itemFeature, setShowFeature } = useContext(
+    FeatureContext
+  );
+
+  return showFeature ? (
+    <Feature
+      {...restProps}
+      src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}
+    >
+      <Content>
+        <FeatureTitle>{itemFeature.title}</FeatureTitle>
+        <FeatureText>{itemFeature.description}</FeatureText>
+        <FeatureClose onClick={() => setShowFeature(false)}>
+          <img src="/images/icons/close.png" alt="Close" />
+        </FeatureClose>
+      </Content>
+
+      <Group margin="30px 0" flexDirection="row" alignItems="center">
+        <Maturity rating={itemFeature.maturity}>
+          {itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}
+        </Maturity>
+        <FeatureText fontWeight="bold">
+          {itemFeature.genre.charAt(0).toUpperCase() +
+            itemFeature.genre.slice(1)}
+        </FeatureText>
+      </Group>
+    </Feature>
+  ) : null;
 };
 
 Card.Entities = function CardEntities({ children, ...restProps }) {
--- netflix/src/components/card/styles/card.js
+++ netflix/src/components/card/styles/card.js
@@ -6,6 +6,26 @@ export const Group = styled.div``;
 
 export const Title = styled.p``;
 
-export const Subtitle = styled.p``;
+export const SubTitle = styled.p``;
 
 export const Text = styled.p``;
+
+export const Feature = styled.div``;
+
+export const FeatureTitle = styled(Title)``;
+
+export const FeatureClose = styled.button``;
+
+export const FeatureText = styled.p``;
+
+export const Maturity = styled.div``;
+
+export const Content = styled.div``;
+
+export const Meta = styled.div``;
+
+export const Entities = styled.div``;
+
+export const Item = styled.div``;
+
+export const Image = styled.img``;
