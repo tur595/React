575455bb6823a879a383f28a325c4ed1c4da28ee
Sinan Senevrensel <tur595@hotmail.com>
2021-04-10 15:47:52 +0200

    header test

--- netflix/src/components/header/index.js
+++ netflix/src/components/header/index.js
@@ -21,7 +21,13 @@ import {
 } from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
-  return bg ? <Background {...restProps}>{children}</Background> : children;
+  return bg ? (
+    <Background {...restProps} data-testid="header-bg">
+      {children}
+    </Background>
+  ) : (
+    children
+  );
 }
 
 Header.Feature = function HeaderFeature({ children, ...restProps }) {
@@ -78,6 +84,7 @@ Header.Search = function HeaderSearch({
     <Search {...restProps}>
       <SearchIcon
         onClick={() => setSearchActive((searchActive) => !searchActive)}
+        data-testid="search-click"
       >
         <img src="/images/icons/search.png" alt="Search" />
       </SearchIcon>
@@ -86,6 +93,7 @@ Header.Search = function HeaderSearch({
         onChange={({ target }) => setSearchTerm(target.value)}
         placeholder="Search films and series"
         active={searchActive}
+        data-testid="search-input"
       />
     </Search>
   );
