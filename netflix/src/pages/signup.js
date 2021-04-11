e929fa38fbf346e1fa91078867bf9c15a353862e
Sinan Senevrensel <tur595@hotmail.com>
2021-04-11 15:26:52 +0200

    signin signup test

--- netflix/src/pages/signup.js
+++ netflix/src/pages/signup.js
@@ -65,7 +65,11 @@ export default function Signup() {
               placeholder="Password"
               onChange={({ target }) => setPassword(target.value)}
             />
-            <Form.Submit disabled={isInvalid} type="submit">
+            <Form.Submit
+              data-testid="sign-up"
+              disabled={isInvalid}
+              type="submit"
+            >
               Sign Up
             </Form.Submit>
             <Form.Text>
