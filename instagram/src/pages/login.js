f10a9fca21ded39f1b38664dbac494a075e7f90c
Sinan Senevrensel <tur595@hotmail.com>
2021-04-15 12:45:40 +0200

    set up constants and routing

---
+++ instagram/src/pages/login.js
@@ -0,0 +1,22 @@
+import { useContext, useState, useEffect } from "react";
+import { useHistory } from "react-router";
+import FirebaseContext from "../context/firebase";
+
+export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "Login - Instagram";
+  }, []);
+
+  return <p>Login</p>;
+}
