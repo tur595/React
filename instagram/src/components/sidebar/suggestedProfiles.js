511e8c344f82f05512bba32cbb4f22b19855ee5d
Sinan <tur595@hotmail.com>
2021-04-21 12:44:31 +0200

    get suggested profiles

---
+++ instagram/src/components/sidebar/suggestedProfiles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SuggestedProfiles({
+  userDocId,
+  username,
+  profileId,
+  userId,
+}) {
+  return (
+    <div>
+      <h1>Suggested Profile {username}</h1>
+    </div>
+  );
+}
