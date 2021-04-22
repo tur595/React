2e3bf5915edaf162358800f503dda9362498f8b8
Sinan <tur595@hotmail.com>
2021-04-22 15:13:49 +0200

    render images

--- instagram/src/components/timeline.js
+++ instagram/src/components/timeline.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 import usePhotos from "../hooks/use-photos";
+import Post from "./post";
 
 function Timeline() {
   const { photos } = usePhotos();
 
-  console.log("photos", photos);
-
-  return <div className="container col-span-2">Timeline</div>;
+  return (
+    <div className="container col-span-2">
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <Post key={content.docId} content={content} />)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos!</p>
+      )}
+    </div>
+  );
 }
 
 export default Timeline;
