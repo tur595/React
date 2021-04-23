7058e035127e65ee5ceef578667a4ae9b7104e7c
Sinan <tur595@hotmail.com>
2021-04-23 12:06:48 +0200

    like/unlike photo

--- instagram/src/components/post/index.js
+++ instagram/src/components/post/index.js
@@ -2,12 +2,23 @@ import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
 import Image from "./image";
+import Actions from "./actions";
 
 export default function Post({ content }) {
+  const commentInput = useRef(null);
+
+  const handleFocus = () => commentInput.current.focus();
+
   return (
     <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
       <Header username={content.username} />
       <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
     </div>
   );
 }
@@ -16,7 +27,7 @@ Post.propTypes = {
   content: PropTypes.shape({
     username: PropTypes.string.isRequired,
     imageSrc: PropTypes.string.isRequired,
-    captions: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
     docId: PropTypes.string.isRequired,
     userLikedPhoto: PropTypes.bool.isRequired,
     likes: PropTypes.array.isRequired,
