98896b3798b42fd11da3217b62404f183248b31d
Sinan <tur595@hotmail.com>
2021-04-24 15:26:15 +0200

    render comments and protected routes

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,6 +4,9 @@ import * as ROUTES from "./constants/routes";
 import UserContext from "./context/user";
 import useAuthListener from "./hooks/use-auth-listener";
 
+import ProtectedRoute from "./helpers/protected-route";
+import IsUserLoggedIn from "./helpers/is-user-logged-in";
+
 const Login = lazy(() => import("./pages/login"));
 const Signup = lazy(() => import("./pages/signup"));
 const Dashboard = lazy(() => import("./pages/dashboard"));
@@ -17,9 +20,23 @@ export default function App() {
       <Router>
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route path={ROUTES.LOGIN} component={Login} />
-            <Route path={ROUTES.SIGN_UP} component={Signup} />
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <IsUserLoggedIn
+              user={user}
+              loggedInPath={ROUTES.DASHBOARD}
+              path={ROUTES.LOGIN}
+            >
+              <Login />
+            </IsUserLoggedIn>
+            <IsUserLoggedIn
+              user={user}
+              loggedInPath={ROUTES.DASHBOARD}
+              path={ROUTES.SIGN_UP}
+            >
+              <Signup />
+            </IsUserLoggedIn>
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+              <Dashboard />
+            </ProtectedRoute>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
--- instagram/src/components/post/add-comment.js
+++ instagram/src/components/post/add-comment.js
@@ -9,7 +9,7 @@ export default function AddComment({
   setComments,
   commentInput,
 }) {
-  const [comment, setcomment] = useState("");
+  const [comment, setComment] = useState("");
   const { firebase, FieldValue } = useContext(FirebaseContext);
   const {
     user: { displayName },
@@ -18,10 +18,53 @@ export default function AddComment({
   const handleSubmitComment = (event) => {
     event.preventDefault();
 
-    return null;
+    setComments([{ displayName, comment }, ...comments]);
+    setComment("");
+
+    return firebase
+      .firestore()
+      .collection("photos")
+      .doc(docId)
+      .update({
+        comments: FieldValue.arrayUnion({ displayName, comment }),
+      });
   };
 
-  return null;
+  return (
+    <div className="border-t border-gray-primary">
+      <form
+        className="flex justify-between pl-0 pr-5"
+        method="post"
+        onSubmit={(event) =>
+          comment.length >= 1
+            ? handleSubmitComment(event)
+            : event.preventDefault()
+        }
+      >
+        <input
+          type="text"
+          aria-label="Add a comment"
+          autoComplete="off"
+          className="text-sm text-gray-base w-full mr-3 py-5 px-4"
+          name="add-comment"
+          placeholder="Add a comment..."
+          value={comment}
+          onChange={({ target }) => setComment(target.value)}
+          ref={commentInput}
+        />
+        <button
+          className={`text-sm font-bold text-blue-medium ${
+            !comment && "opacity-25"
+          }`}
+          type="button"
+          disabled={comment.length < 1}
+          onClick={handleSubmitComment}
+        >
+          Post
+        </button>
+      </form>
+    </div>
+  );
 }
 
 AddComment.propTypes = {
