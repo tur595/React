961243e744514848ac93af105be8c97e68565433
Sinan <tur595@hotmail.com>
2021-05-09 14:16:44 +0200

    screen responsiveness

--- hulu/components/Nav.js
+++ hulu/components/Nav.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 export default function Nav() {
   const router = useRouter();
   return (
-    <nav className="relative ">
+    <nav className="relative">
       <div
         className="flex px-10 sm:px-20 text-2xl whitespace-nowrap 
       space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide"
