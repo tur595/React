96a21adee67fb3a7f190a0557bc954fa245e4cb0
Sinan <tur595@hotmail.com>
2021-05-12 13:22:20 +0200

    sidebar component

--- todoist/src/App.js
+++ todoist/src/App.js
@@ -1,10 +1,12 @@
+import { Content } from "./components/layout/Content";
 import { Header } from "./components/layout/Header";
 
 export const App = () => {
   return (
     <div className="App">
+      <p>Todoist</p>
       <Header />
-      <h1>Todoist</h1>
+      <Content />
     </div>
   );
 };
--- todoist/src/components/layout/Header.js
+++ todoist/src/components/layout/Header.js
@@ -1,7 +1,21 @@
+import { FaPizzaSlice } from "react-icons/fa";
+
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/logo.png" alt="Todoist" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li>+</li>
+            <li>
+              <FaPizzaSlice />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
