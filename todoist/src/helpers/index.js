03830a56aac618e944e159df4a9b50dab9c1f0d5
Sinan <tur595@hotmail.com>
2021-05-13 12:50:22 +0200

    extended hooks

--- todoist/src/helpers/index.js
+++ todoist/src/helpers/index.js
@@ -1,4 +1,4 @@
 import { collatedTasks } from "../constants";
 
 export const collatedTasksExists = (selectedProject) =>
-  collatedTasksExists.find((task) => task.key === selectedProject);
+  collatedTasks.find((task) => task.key === selectedProject);
--- todoist/src/hooks/index.js
+++ todoist/src/hooks/index.js
@@ -5,6 +5,7 @@ import { collatedTasksExists } from "../helpers";
 
 export const useTasks = (selectedProject) => {
   const [tasks, setTasks] = useState([]);
+  const [archivedTasks, setArchivedTasks] = useState([]);
 
   useEffect(() => {
     let unsubcsribe = firebase
@@ -24,5 +25,53 @@ export const useTasks = (selectedProject) => {
         : selectedProject === "INBOX" || selectedProject === 0
         ? (unsubcsribe = unsubcsribe.where("date", "==", ""))
         : unsubcsribe;
+
+    unsubcsribe = unsubcsribe.onSnapshot((snapshot) => {
+      const newTasks = snapshot.docs.map((task) => ({
+        id: task.id,
+        ...task.data(),
+      }));
+
+      setTasks(
+        selectedProject === "NEXT_7"
+          ? newTasks.filter(
+              (task) =>
+                moment(task.date, "DD-MM-YYYY").diff(moment(), "days") <= 7 &&
+                task.archived !== true
+            )
+          : newTasks.filter((task) => task.archived !== true)
+      );
+
+      setArchivedTasks(newTasks.filter((task) => task.archived !== false));
+    });
+
+    return () => unsubscribe();
   }, [selectedProject]);
+
+  return { tasks, archivedTasks };
+};
+
+export const useProjects = () => {
+  const [projects, setProjets] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("projects")
+      .where("userId", "==", "asd123")
+      .orderBy("projectId")
+      .get()
+      .then((snapshot) => {
+        const allProjects = snapshot.docs.map((project) => ({
+          ...project.data(),
+          docID: project.id,
+        }));
+
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjets(allProjects);
+        }
+      });
+  }, [projects]);
+
+  return { projects, setProjets };
 };
