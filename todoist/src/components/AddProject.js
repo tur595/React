56c924cc414937157c4424ecb058f42af5d07936
Sinan <tur595@hotmail.com>
2021-05-18 13:18:06 +0200

    add project component

--- todoist/src/components/AddProject.js
+++ todoist/src/components/AddProject.js
@@ -1,7 +1,63 @@
-export const AddProject = () => {
+import { useState } from "react";
+import { useProjectsValue } from "../context";
+import { firebase } from "../firebase";
+import { generatePushId } from "../helpers";
+
+export const AddProject = ({ shouldShow = false }) => {
+  const [show, setShow] = useState(shouldShow);
+  const [projectName, setProjectName] = useState("");
+  const projectId = generatePushId();
+  const { setProjects } = useProjectsValue();
+
+  const addProject = () =>
+    projectName &&
+    firebase
+      .firestore()
+      .collection("projects")
+      .add({ projectId, name: projectName, userId: "asd123" })
+      .then(() => {
+        setProjects([]);
+        setProjectName("");
+        setShow(false);
+      });
+
   return (
-    <div>
-      <p>Add project</p>
+    <div className="add-project" data-testid="add-project">
+      {show && (
+        <div className="add-project__input">
+          <input
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+            className="add-project__name"
+            data-testid="project-name"
+            type="text"
+            placeholder="Name your project"
+          />
+          <button
+            className="add-project__submit"
+            type="button"
+            onClick={() => addProject()}
+            data-testid="add-project-submit"
+          >
+            Add Project
+          </button>
+          <span
+            data-testid="hide-project-overlay"
+            className="add-project__cancel"
+            onClick={() => setShow(false)}
+          >
+            Cancel
+          </span>
+        </div>
+      )}
+      <span className="add-project__plus">{!show && "+ "}</span>
+      <span
+        data-testid="add-project-action"
+        className="add-project__text"
+        onClick={() => setShow(!show)}
+      >
+        {!show && "Add Project"}
+      </span>
     </div>
   );
 };
