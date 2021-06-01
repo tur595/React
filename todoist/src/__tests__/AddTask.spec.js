15c52843469ef7a276a2f29130ce5aff63ebbcfc
Sinan <tur595@hotmail.com>
2021-06-01 15:17:11 +0200

    testing <AddTask />

--- todoist/src/__tests__/AddTask.spec.js
+++ todoist/src/__tests__/AddTask.spec.js
@@ -349,5 +349,86 @@ describe("<AddTask />", () => {
 
       fireEvent.click(queryByTestId("add-task"));
     });
+    it("renders <AddTask /> and adds a task with a task date of TOMORROW", () => {
+      useSelectedProjectValue.mockImplementation(() => ({
+        selectedProject: "1",
+      }));
+
+      const { queryByTestId } = render(<AddTask showMain />);
+      fireEvent.click(queryByTestId("show-main-action"));
+      expect(queryByTestId("add-task-content")).toBeTruthy();
+      expect(queryByTestId("add-task-main")).toBeTruthy();
+
+      fireEvent.change(queryByTestId("add-task-content"), {
+        target: { value: "I am the most amazing task ever!" },
+      });
+      expect(queryByTestId("add-task-content").value).toBe(
+        "I am the most amazing task ever!"
+      );
+
+      fireEvent.click(queryByTestId("show-task-date-overlay"));
+      expect(queryByTestId("task-date-overlay")).toBeTruthy();
+
+      fireEvent.click(queryByTestId("task-date-tomorrow"));
+      expect(queryByTestId("task-date-overlay")).toBeFalsy();
+
+      fireEvent.click(queryByTestId("show-task-date-overlay"));
+      expect(queryByTestId("task-date-overlay")).toBeTruthy();
+
+      fireEvent.keyDown(queryByTestId("task-date-tomorrow"), {
+        key: "a",
+        code: 65,
+      });
+      expect(queryByTestId("task-date-overlay")).toBeTruthy();
+
+      fireEvent.keyDown(queryByTestId("task-date-tomorrow"), {
+        key: "Enter",
+        code: 13,
+      });
+      expect(queryByTestId("task-date-overlay")).toBeFalsy();
+
+      fireEvent.click(queryByTestId("add-task"));
+    });
+
+    it("renders <AddTask /> and adds a task with a task date of NEXT_7", () => {
+      useSelectedProjectValue.mockImplementation(() => ({
+        selectedProject: "1",
+      }));
+
+      const { queryByTestId } = render(<AddTask showMain />);
+      fireEvent.click(queryByTestId("show-main-action"));
+      expect(queryByTestId("add-task-content")).toBeTruthy();
+      expect(queryByTestId("add-task-main")).toBeTruthy();
+
+      fireEvent.change(queryByTestId("add-task-content"), {
+        target: { value: "I am the most amazing task ever!" },
+      });
+      expect(queryByTestId("add-task-content").value).toBe(
+        "I am the most amazing task ever!"
+      );
+
+      fireEvent.click(queryByTestId("show-task-date-overlay"));
+      expect(queryByTestId("task-date-overlay")).toBeTruthy();
+
+      fireEvent.click(queryByTestId("task-date-next-week"));
+      expect(queryByTestId("task-date-overlay")).toBeFalsy();
+
+      fireEvent.click(queryByTestId("show-task-date-overlay"));
+      expect(queryByTestId("task-date-overlay")).toBeTruthy();
+
+      fireEvent.keyDown(queryByTestId("task-date-next-week"), {
+        key: "a",
+        code: 65,
+      });
+      expect(queryByTestId("task-date-overlay")).toBeTruthy();
+
+      fireEvent.keyDown(queryByTestId("task-date-next-week"), {
+        key: "Enter",
+        code: 13,
+      });
+      expect(queryByTestId("task-date-overlay")).toBeFalsy();
+
+      fireEvent.click(queryByTestId("add-task"));
+    });
   });
 });
