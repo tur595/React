c03c53b539e430b69dcbefe8f38f88c2be1a4505
Sinan <tur595@hotmail.com>
2021-06-12 11:35:14 +0200

    refactor stories

--- newsreader/src/App.js
+++ newsreader/src/App.js
@@ -1,16 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { getStoryIds } from "./services/hnApi";
+import { StoriesContainer } from "./containers/StoriesContainer";
 
 export const App = () => {
-  const [storyIds, setStoryIds] = useState([]);
-
-  useEffect(() => {
-    getStoryIds().then((data) => setStoryIds(data));
-  }, []);
-
-  return (
-    <div className="App">
-      <p>{JSON.stringify(storyIds)}</p>
-    </div>
-  );
+  return <StoriesContainer />;
 };
---
+++ newsreader/src/containers/StoriesContainer.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import { getStoryIds } from "../services/hnApi";
+
+export const StoriesContainer = () => {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then((data) => setStoryIds(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <p>{JSON.stringify(storyIds)}</p>
+    </div>
+  );
+};
