eae7660325904752367d26cc53bd0f10b0c8a9ab
Sinan <tur595@hotmail.com>
2021-06-20 13:49:40 +0200

    testing <App />

---
+++ newsreader/src/__tests__/App.spec.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { App } from "../App";
+import { storyIds, singularStory } from "../fixtures";
+import { getStory, getStoryIds } from "../services/hnApi";
+import { useInfiniteScroll } from "../hooks/useInfiniteScroll";
+import { STORY_INCREMENT } from "../constants";
+import {
+  cleanup,
+  getByText,
+  render,
+  waitForElement,
+} from "@testing-library/react";
+
+beforeEach(cleanup);
+
+jest.mock("../hooks/useInfiniteScroll.js");
+jest.mock("../services/hnApi", () => ({
+  getStory: jest.fn(),
+  getStoryIds: jest.fn(),
+}));
+
+test("renders the application", async () => {
+  useInfiniteScroll.mockImplementation(() => ({
+    count: STORY_INCREMENT,
+  }));
+
+  getStory.mockImplementation(() => Promise.resolve(singularStory));
+  getStoryIds.mockImplementation(() => Promise.resolve(storyIds));
+
+  const { getByText, queryByTestId } = render(<App />);
+  await waitForElement(() => [
+    expect(getByText("Hacker News Stories")).toBeTruthy(),
+    expect(getByText("Tarnished: Google Responds")).toBeTruthy(),
+    expect(queryByTestId("story-by").textContent).toEqual("By: John Doe"),
+  ]);
+});
