f02f3a97756289bdea50f500bca9c3dff81bb5f3
Sinan <tur595@hotmail.com>
2021-06-25 12:11:41 +0200

    retrieve pokemon data

--- pokemon-graphql/src/App.js
+++ pokemon-graphql/src/App.js
@@ -1,10 +1,19 @@
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { PokemonsContainer } from "./containers/PokemonsContainer";
+
 function App() {
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon2.vercel.app",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <ApolloProvider client={client}>
+      <main>
         <p>Pokemon</p>
-      </header>
-    </div>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
   );
 }
 
---
+++ pokemon-graphql/src/containers/PokemonsContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_POKEMONS } from "../graphql/get-pokemons";
+
+export const PokemonsContainer = () => {
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 9 },
+  });
+
+  return (
+    <div className="pokemons">
+      {pokemons && pokemons.map((pokemon) => JSON.stringify(pokemon))}
+    </div>
+  );
+};
