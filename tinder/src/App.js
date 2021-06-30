0322b43454bf0746043c4aae1a46e73ae49fb156
Sinan <tur595@hotmail.com>
2021-06-30 12:22:59 +0200

    main app structure

--- tinder/src/App.js
+++ tinder/src/App.js
@@ -1,9 +1,47 @@
 import "./App.css";
+import Header from "./components/Header";
+import Person from "./components/Person";
+import Lonely from "./components/Lonely";
+import data from "./data.json";
+import { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [likedUsers, setLikedUsers] = useState([]);
+  const [superLikedUsers, setSuperLikedUsers] = useState([]);
+  const [dislikedUsers, setDislikedUsers] = useState([]);
+  const activeUser = 0;
+
+  switch (action) {
+    case "ADD_TO_LIKED_USER":
+      break;
+    case "ADD_TO_DISLIKED_USER":
+      break;
+    case "ADD_TO_SUPERLIKED_USER":
+
+    default:
+      return people;
+  }
+
   return (
     <div className="App">
+      <Header />
       <p>Tinder</p>
+      {people[1] ? (
+        <Person
+          key={person[1].id}
+          person={people[1]}
+          modifySuperficialChoices={modifySuperficialChoices}
+          likedUsers={likedUsers}
+        />
+      ) : (
+        <Lonely
+          activeUserImage={people[activeUser].image}
+          likedUsers={likedUsers}
+          superLikedUsers={superLikedUsers}
+        />
+      )}
+      )
     </div>
   );
 }
