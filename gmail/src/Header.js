4262004a8cfb6b85f30f989420de95c17474701c
Sinan <tur595@hotmail.com>
2021-07-20 11:20:54 +0200

    <Header />

---
+++ gmail/src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { IconButton } from "@material-ui/core";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt=""
+        />
+      </div>
+
+      <div className="header__middle"></div>
+
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
