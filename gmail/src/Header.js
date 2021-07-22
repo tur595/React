dc9c9c35d47d283934138a25d08873251701782b
Sinan <tur595@hotmail.com>
2021-07-22 11:36:52 +0200

    <Header />

--- gmail/src/Header.js
+++ gmail/src/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Header.css";
 import MenuIcon from "@material-ui/icons/Menu";
-import { IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar, IconButton } from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
@@ -15,10 +19,21 @@ function Header() {
           alt=""
         />
       </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input type="text" placeholder="Search mail" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
 
-      <div className="header__middle"></div>
-
-      <div className="header__right"></div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar className="avatar" />
+      </div>
     </div>
   );
 }
