43165fcab400549ff72116d2ca20504d0738babf
Sinan <tur595@hotmail.com>
2021-07-27 14:36:04 +0200

    <EmailList />

--- gmail/src/EmailList.js
+++ gmail/src/EmailList.js
@@ -1,10 +1,46 @@
 import React from "react";
 import "./EmailList.css";
+import { Checkbox, IconButton } from "@material-ui/core";
+import RedoIcon from "@material-ui/icons/Redo";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function EmailList() {
   return (
-    <div>
-      <p>Emaillist</p>
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="emailList__sections"></div>
     </div>
   );
 }
