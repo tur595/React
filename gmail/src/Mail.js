2fef3f2875169ae4c3f112e3987f338f791a5576
Sinan <tur595@hotmail.com>
2021-08-01 17:16:56 +0200

    <Mail />

--- gmail/src/Mail.js
+++ gmail/src/Mail.js
@@ -1,10 +1,85 @@
+import { IconButton } from "@material-ui/core";
+import {
+  ArrowBack,
+  CheckCircle,
+  Delete,
+  Email,
+  Error,
+  ExitToApp,
+  LabelImportant,
+  MoreVert,
+  MoveToInbox,
+  Print,
+  UnfoldMore,
+  WatchLater,
+} from "@material-ui/icons";
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./Mail.css";
 
 function Mail() {
+  const history = useHistory();
+
   return (
-    <div>
-      <p>Mail</p>
+    <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton onClick={() => history.push("/")}>
+            <ArrowBack />
+          </IconButton>
+
+          <IconButton>
+            <MoveToInbox />
+          </IconButton>
+
+          <IconButton>
+            <Error />
+          </IconButton>
+
+          <IconButton>
+            <Delete />
+          </IconButton>
+
+          <IconButton>
+            <Email />
+          </IconButton>
+
+          <IconButton>
+            <WatchLater />
+          </IconButton>
+
+          <IconButton>
+            <CheckCircle />
+          </IconButton>
+
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+        <div className="mail__toolsRight">
+          <IconButton>
+            <UnfoldMore />
+          </IconButton>
+
+          <IconButton>
+            <Print />
+          </IconButton>
+
+          <IconButton>
+            <ExitToApp />
+          </IconButton>
+        </div>
+      </div>
+      <div className="mail__body">
+        <div className="mail__bodyHeader">
+          <h2>Subject</h2>
+          <LabelImportant className="mail__important" />
+          <p>Title</p>
+          <p>10pm</p>
+        </div>
+
+        <div className="mail__message">Message</div>
+      </div>
     </div>
   );
 }
