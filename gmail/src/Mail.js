0177a88a104c04dc6b914759443e2f0acfa2db74
Sinan <tur595@hotmail.com>
2021-08-10 11:26:56 +0200

    render email content

--- gmail/src/Mail.js
+++ gmail/src/Mail.js
@@ -14,11 +14,14 @@ import {
   WatchLater,
 } from "@material-ui/icons";
 import React from "react";
+import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import "./Mail.css";
+import { selectOpenMail } from "./features/mailSlice";
 
 function Mail() {
   const history = useHistory();
+  const selectedMail = useSelector(selectOpenMail);
 
   return (
     <div className="mail">
@@ -72,14 +75,14 @@ function Mail() {
       </div>
       <div className="mail__body">
         <div className="mail__bodyHeader">
-          <h2>Subject</h2>
+          <h2>{selectedMail?.subject}</h2>
           <LabelImportant className="mail__important" />
-          <p>Title</p>
-          <p className="mail__time">10pm</p>
+          <p>{selectedMail?.title}</p>
+          <p className="mail__time">{selectedMail?.time}</p>
         </div>
 
         <div className="mail__message">
-          <p>Message</p>
+          <p>{selectedMail?.description}</p>
         </div>
       </div>
     </div>
