3cc5a4adcfa0ec90fc4daf2563ee9e2b51c95093
Sinan <tur595@hotmail.com>
2021-09-13 12:24:08 +0200

    header bottom and banner

--- amazon/src/pages/index.js
+++ amazon/src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -9,6 +10,10 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+      </main>
     </div>
   );
 }
