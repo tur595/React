a75d1c07e83b2fdd0c61e5005a67fff7999f81f5
Sinan <tur595@hotmail.com>
2021-09-20 13:20:22 +0200

    checkout page and routing

--- amazon/src/components/Header.js
+++ amazon/src/components/Header.js
@@ -5,15 +5,18 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 
 function Header() {
   const [session, loading] = useSession();
+  const router = useRouter();
 
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -49,7 +52,10 @@ function Header() {
             <p className="ml-2">Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="relative link flex items-center">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative link flex items-center"
+          >
             <span
               className="absolute top-0 right-0 md:right-12 h-4 w-4 bg-yellow-400
             text-center rounded-full text-black font-bold"
