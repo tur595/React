0d381b9432582f2885c20ec3153ea69d2161eb0c
Sinan <tur595@hotmail.com>
2021-10-12 14:44:48 +0200

    header left

---
+++ airbnb/components/Header.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      <div></div>
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
--- airbnb/pages/index.js
+++ airbnb/pages/index.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Airbnb</h1>
+
+      <Header />
     </div>
   );
 }
