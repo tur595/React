6fb7bce84a7581869e865ef17db53f1051f53270
Sinan <tur595@hotmail.com>
2021-10-13 11:20:39 +0200

    header middle

--- airbnb/components/Header.js
+++ airbnb/components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
@@ -11,7 +12,15 @@ function Header() {
           objectPosition="left"
         />
       </div>
-      <div></div>
+
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2 " />
+      </div>
       <div></div>
     </header>
   );
