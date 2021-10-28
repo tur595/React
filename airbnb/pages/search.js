7d96104f4ab094f5e11d9c7791eef8a9606425f7
Sinan <tur595@hotmail.com>
2021-10-28 14:41:23 +0200

    search page

---
+++ airbnb/pages/search.js
@@ -0,0 +1,28 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Search() {
+  return (
+    <div className="select-none">
+      <Header />
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">300+ Stays for 5 number of guests</p>
+
+          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Oslo</h1>
+
+          <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexiblity</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More filters</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
