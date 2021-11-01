68ae12313406d0ce575cdafdc94ba0664b2fa8e2
Sinan <tur595@hotmail.com>
2021-11-01 12:52:14 +0100

    pass search parameters

--- airbnb/pages/search.js
+++ airbnb/pages/search.js
@@ -1,15 +1,27 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useRouter } from "next/dist/client/router";
+import { format } from "date-fns";
 
 function Search() {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+
   return (
     <div className="select-none">
       <Header />
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
-          <p className="text-xs">300+ Stays for 5 number of guests</p>
+          <p className="text-xs">
+            300+ Stays between {range} for {noOfGuests} guests
+          </p>
 
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Oslo</h1>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
 
           <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button">Cancellation Flexiblity</p>
