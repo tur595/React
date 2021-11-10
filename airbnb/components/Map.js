64d7b516ec190bda0453cae91aba911dc9a3b629
Sinan <tur595@hotmail.com>
2021-11-10 14:31:29 +0100

    initialize map

---
+++ airbnb/components/Map.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
+function Map() {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/tur595/ckvtjf9e322ev15rwdbydmnu8"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+    ></ReactMapGL>
+  );
+}
+
+export default Map;
--- airbnb/next.config.js
+++ airbnb/next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoidHVyNTk1IiwiYSI6ImNrdnRqNWgwdDFicG4ycnFwcnE3NmJlc28ifQ.u-CLstDZNaugbSZZm2t6oA",
+  },
 };
