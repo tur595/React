f80475833227e93ec87d17ca0a29695939817600
Sinan Senevrensel <tur595@hotmail.com>
2021-11-29 15:43:38 +0300

    initial commit

---
+++ spotify2.0/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify 2.0</title>
+        <link rel="icon" href="/spotify.png" />
+      </Head>
+    <h1>Spotify 2.0</h1>
+
+    <main>
+      {/* Sidebar */}
+      {/* Center */}
+    </main>
+
+    <div>
+      {/* Player */}
+    </div>
+    </div>
+  )
+}
