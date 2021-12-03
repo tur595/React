045750b0906f9d510795efb0672b7288b5768245
Sinan Senevrensel <tur595@hotmail.com>
2021-12-03 18:08:52 +0300

    sidebar component

--- spotify2.0/pages/index.js
+++ spotify2.0/pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
     <div className="">
       <Head>
         <title>Spotify 2.0</title>
-        <link rel="icon" href="/spotify.png" />
       </Head>
-    <h1>Spotify 2.0</h1>
 
     <main>
-      {/* Sidebar */}
+      <Sidebar />
       {/* Center */}
     </main>
 
