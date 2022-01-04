92f9a22fd37d76077b8f18c19d2eaeaca4ab019e
Sinan Senevrensel <tur595@hotmail.com>
2022-01-04 18:35:35 +0300

    retrieve songs

--- spotify2.0/components/Center.js
+++ spotify2.0/components/Center.js
@@ -36,7 +36,7 @@ function Center() {
     }, [spotifyApi, playlistId])
 
     return (
-        <div className="flex-grow">
+        <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
             <header className="absolute top-5 right-8">
                 <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
                     <img className="rounded-full w-10 h-10" src={session?.user.image} alt="" />
@@ -45,12 +45,8 @@ function Center() {
                 </div>
                 
             </header>
-            <section className={`flex items-end space-x-7 bg-gradient-to-b ${color} to-black h-80 text-white p-8`}>
-                
-            <ColorExtractor getColors={imageColors => console.log(imageColors)} >
+            <section className={`flex items-end space-x-7 bg-gradient-to-b ${color} to-black h-80 text-white p-8`}>        
                     <img className="h-44 w-44 shadow-2xl" src={playlist?.images?.[0]?.url} alt="" />
-            </ColorExtractor>
-                
                 <div>
                     <p>PLAYLIST</p>
                     <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
