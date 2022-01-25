c81a23bedea609724f1dd5dafba10d2bab6a1277
Sinan Senevrensel <tur595@hotmail.com>
2022-01-25 15:46:18 +0300

    initial commit

---
+++ metaverse/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Metaverse</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <h1 className=''>Metaverse</h1>
+     
+    </div>
+  )
+}
