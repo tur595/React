e6039981c026ade1f3f71d4ae2a3733659ab329f
Sinan Senevrensel <tur595@hotmail.com>
2022-02-26 11:55:32 +0300

    authentication

--- metaverse/components/Login.js
+++ metaverse/components/Login.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 function Login() {
+  const { authenticate } = useMoralis();
+
   return <div className="bg-black relative">
       <h1>Login</h1>
       <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
@@ -9,7 +12,7 @@ function Login() {
             className="rounded-full"
           />
 
-          <button className="bg-blue-300 rounded-lg p-5 font-bold animate-pulse">Login to the Metaverse</button>
+          <button onClick={authenticate} className="bg-blue-300 rounded-lg p-5 font-bold animate-pulse">Login to the Metaverse</button>
       </div>
 
       <div className="w-full h-screen">
