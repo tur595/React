68adc66338c34ce8684ce057bb2582d288013799
Sinan Senevrensel <tur595@hotmail.com>
2022-03-12 16:01:25 +0300

    Header component

--- medium/tailwind.config.js
+++ medium/tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
