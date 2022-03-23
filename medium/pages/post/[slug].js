9f2f2ffb483743cd6acd96766cfdec916307837d
Sinan Senevrensel <tur595@hotmail.com>
2022-03-23 13:04:24 +0300

    post page

--- medium/pages/post/[slug].js
+++ medium/pages/post/[slug].js
@@ -1,11 +1,65 @@
+/* eslint-disable @next/next/no-img-element */
+import PortableText from "react-portable-text";
 import Header from "../../components/Header";
-import { sanityClient } from "../../sanity";
+import { sanityClient, urlFor } from "../../sanity";
 
 function Post({ post }) {
   console.log(post);
   return (
     <main>
       <Header />
+      {post.mainImage && (
+        <img
+          className="w-full h-40 object-cover"
+          src={urlFor(post.mainImage).url()}
+          alt=""
+        />
+      )}
+
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          {post.mainImage && (
+            <img
+              className="h-10 w-10 rounded-full"
+              src={urlFor(post.author.image).url()}
+              alt=""
+            />
+          )}
+
+          <p className="font-extralight text-sm">
+            Blog post by{" "}
+            <span className="text-green-500">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+            serializers={{
+              h1: (props) => {
+                <h1 className="text-2xl font-bold my-5" {...props} />;
+              },
+              h2: (props) => {
+                <h2 className="text-xl font-bold my-5" {...props} />;
+              },
+              li: ({ children }) => {
+                <li className="ml-4 list-disc">{children}</li>;
+              },
+              link: ({ href, children }) => {
+                <a className="text-blue-500 hover:underline">{children}</a>;
+              },
+            }}
+          />
+        </div>
+      </article>
     </main>
   );
 }
