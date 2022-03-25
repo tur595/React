14c426710b773075792e615eab32064f1ee35f36
Sinan Senevrensel <tur595@hotmail.com>
2022-03-25 15:17:37 +0300

    comment section

--- medium/pages/post/[slug].js
+++ medium/pages/post/[slug].js
@@ -1,10 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
+import { useForm } from "react-hook-form";
 import PortableText from "react-portable-text";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 
 function Post({ post }) {
-  console.log(post);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <main>
       <Header />
@@ -60,6 +66,59 @@ function Post({ post }) {
           />
         </div>
       </article>
+
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+
+      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="py-3 mt-2" />
+
+        <input type="hidden" name="_id" value={post._id} {...register("_id")} />
+
+        <label className="block mb-5">
+          <span className="text-gray-700">Name</span>
+          <input
+            {...register("name", { required: true })}
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring ring-1"
+            placeholder="John Appleseed"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Email</span>
+          <input
+            {...register("email", { required: true })}
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring ring-1"
+            placeholder="john.appleseed@xyz.com"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            {...register("comment", { required: true })}
+            className="shadow border rounded py-2 px-3 from-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring ring-1"
+            placeholder="Pretty nice article..."
+            rows={8}
+          />
+        </label>
+
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500">* Name field is required</span>
+          )}
+          {errors.email && (
+            <span className="text-red-500">* Email field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500">* Comment field is required</span>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="shadow bg-yellow-500 hover:bg-yellow-400 cursor-pointer focus:shadow-outline focus:outline-none text-white font-bold px-4 py-2 rounded"
+        />
+      </form>
     </main>
   );
 }
