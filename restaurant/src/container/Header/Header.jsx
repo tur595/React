5d91a3e650445f2d5f8169341f74b79ec86024ef
Sinan Senevrensel <tur595@hotmail.com>
2022-04-11 21:16:00 +0300

    header section

--- restaurant/src/container/Header/Header.jsx
+++ restaurant/src/container/Header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app_wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
+        cupiditate. Perspiciatis rerum aut veniam dolores iusto possimus eum
+        architecto beatae?
+      </p>
+      <button type="button" className="custom__button">
+        Explore menu
+      </button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
