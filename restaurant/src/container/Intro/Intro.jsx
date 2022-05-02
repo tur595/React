7dab65cbdd92b35b961985ecb1c3a80dc183313c
Sinan Senevrensel <tur595@hotmail.com>
2022-05-02 22:14:24 +0300

    video section

--- restaurant/src/container/Intro/Intro.jsx
+++ restaurant/src/container/Intro/Intro.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
+import "./Intro.css";
 
-import './Intro.css';
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const [transparency, setTransparency] = useState(null);
+  const vidRef = useRef();
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+    if (playVideo) {
+      vidRef.current.pause();
+      setTransparency({ background: "rgba(0, 0, 0, 0.3)" });
+    } else {
+      vidRef.current.play();
+      setTransparency({ background: "rgba(0, 0, 0, 0)" });
+    }
+  };
+
+  return (
+    <div className="app__video">
+      <video
+        ref={vidRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center" style={transparency}>
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
